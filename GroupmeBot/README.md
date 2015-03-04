260e26d1725123a259dea6fc07cea70acb266ed8
Chris Barry <cwb@outlook.com>
2015-03-04 17:38:16 -0500

    Added groupmebot

---
+++ GroupmeBot/README.md
@@ -0,0 +1,11 @@
+#GroupMe Bot
+Currently using the npm fancy-groupme-bot, from here: <https://www.npmjs.com/package/fancy-groupme-bot>
+
+#GroupMe Info
+
+Redirect URL:    <https://oauth.groupme.com/oauth/authorize?client_id=RXgQ3269W78Bx1U1SPcOhtFNkbnwBo4lC8K236XgzAMjr2XM>
+Callback URL:    <localhost>
+
+Chris Barry's 
+Access Token:    0e088af0a4b301327e9a16a8358f1ba3
+55JqxdWFl4G29PSPDcp6O7Zc2grs74giP3wDIIXz
---
+++ GroupmeBot/botdemo.js
@@ -0,0 +1,32 @@
+var bot = require('fancy-groupme-bot');
+
+// local configuration read from env.
+var TOKEN = "0e088af0a4b301327e9a16a8358f1ba3"; // your groupme api token
+var USER_TOKEN = "55JqxdWFl4G29PSPDcp6O7Zc2grs74giP3wDIIXz";
+var GROUP = "12717380"; // the room you want to join
+var NAME = "No1"; // the name of your bot
+var URL = "http://localhost:8000"; // the domain you're serving from, should be accessible by Groupme.
+
+const CONFIG = {
+	token: TOKEN,
+	group: GROUP,
+	name: NAME,
+	url: URL
+};
+
+var mybot = bot(CONFIG);
+
+mybot.on('botRegistered', function(b) {
+	console.log("I am registered");
+	b.message("What's up guys?");
+});
+
+mybot.on('botMessage', function(b, message) {
+	console.log("I got a message, fyi");
+	if (message.name != b.name) {
+		b.message(message.name + " said " + message.text);
+	}
+});
+
+console.log("i am serving");
+mybot.serve(8000);
