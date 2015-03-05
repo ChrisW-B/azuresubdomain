7bc2359d3f851446a3194ca8bc1f2a35096b4f93
Chris Barry <cwb@outlook.com>
2015-03-04 21:48:57 -0500

    Back to the basics I guess

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
-var bot = require('fancy-groupme-bot');
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function(req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+	res.end('Hello World\nPort is ' + port);
+}).listen(port);
 
-// local configuration read from env.
-var TOKEN = "0e088af0a4b301327e9a16a8358f1ba3"; // your groupme api token
-var USER_TOKEN = "55JqxdWFl4G29PSPDcp6O7Zc2grs74giP3wDIIXz";
-var GROUP = "12717380"; // the room you want to join
-var NAME = "No1"; // the name of your bot
-var URL = "http://azure.chriswbarry.com:8000/GroupmeBot"; // the domain you're serving from, should be accessible by Groupme.
+// var bot = require('fancy-groupme-bot');
 
-const CONFIG = {
-	token: TOKEN,
-	group: GROUP,
-	name: NAME,
-	url: URL
-};
+// // local configuration read from env.
+// var TOKEN = "0e088af0a4b301327e9a16a8358f1ba3"; // your groupme api token
+// var USER_TOKEN = "55JqxdWFl4G29PSPDcp6O7Zc2grs74giP3wDIIXz";
+// var GROUP = "12717380"; // the room you want to join
+// var NAME = "No1"; // the name of your bot
+// var URL = "http://azure.chriswbarry.com:8000/GroupmeBot"; // the domain you're serving from, should be accessible by Groupme.
 
-var mybot = bot(CONFIG);
+// const CONFIG = {
+// 	token: TOKEN,
+// 	group: GROUP,
+// 	name: NAME,
+// 	url: URL
+// };
 
-mybot.on('botRegistered', function(b) {
-	console.log("I am registered");
-	b.message("What's up guys?");
-});
+// var mybot = bot(CONFIG);
 
-mybot.on('botMessage', function(b, message) {
-	console.log("I got a message, fyi");
-	if (message.name != b.name) {
-		b.message(message.name + " said " + message.text);
-	}
-});
+// mybot.on('botRegistered', function(b) {
+// 	console.log("I am registered");
+// 	b.message("What's up guys?");
+// });
 
-console.log("i am serving");
-var port = process.env.PORT || 1337;
-mybot.serve(8000);
+// mybot.on('botMessage', function(b, message) {
+// 	console.log("I got a message, fyi");
+// 	if (message.name != b.name) {
+// 		b.message(message.name + " said " + message.text);
+// 	}
+// });
+
+// console.log("i am serving");
+// var port = process.env.PORT || 1337;
+// mybot.serve(8000);
