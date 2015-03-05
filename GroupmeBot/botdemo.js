f50d2cbab9e39ea87734b8ce3c18e4dc4a055433
Chris Barry <cwb@outlook.com>
2015-03-04 20:30:51 -0500

    fixed bot location

--- GroupmeBot/botdemo.js
+++ GroupmeBot/botdemo.js
@@ -5,7 +5,7 @@ var TOKEN = "0e088af0a4b301327e9a16a8358f1ba3"; // your groupme api token
 var USER_TOKEN = "55JqxdWFl4G29PSPDcp6O7Zc2grs74giP3wDIIXz";
 var GROUP = "12717380"; // the room you want to join
 var NAME = "No1"; // the name of your bot
-var URL = "http://localhost:8000"; // the domain you're serving from, should be accessible by Groupme.
+var URL = "http://azure.chriswbarry.com:8000/GroupmeBot"; // the domain you're serving from, should be accessible by Groupme.
 
 const CONFIG = {
 	token: TOKEN,
@@ -29,5 +29,4 @@ mybot.on('botMessage', function(b, message) {
 });
 
 console.log("i am serving");
-var port = process.env.PORT || 1337;
-mybot.serve(port);
+mybot.serve(8000);
